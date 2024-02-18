42581adae29313b9c656e8b5aa6c5743c111cd5f
NARD <110368170+CocoShesh@users.noreply.github.com>
2024-02-18 12:55:22 +0800

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,17 +6,17 @@ const Contact = () => {
   return (
     <>
       <section
-        className="h-screen flex flex-col justify-center items-center"
+        className="h-screen max-w-screen mx-5 flex flex-col justify-center items-center"
         id="contact"
       >
-        <h1 className="font-Mono text-3xl md:text-4xl lg:text-3xl xl:text-3xl mb-5">
+        <h1 className="font-Mono sm:text-xl md:text-4xl lg:text-3xl xl:text-3xl ">
           04. Contact
         </h1>
-        <p className="text-5xl md:text-6xl lg:text-7xl xl:text-5xl font-Noto my-5 font-bold text-[#ccd6f6]">
+        <p className="text-4xl md:text-6xl lg:text-7xl xl:text-5xl font-Noto my-3 font-bold text-[#ccd6f6]">
           Get In Touch
         </p>
 
-        <p className="w-full sm:w-[500px] text-center text-lg md:text-xl lg:text-2xl xl:text-lg font-Noto text-[#67738e] pb-14">
+        <p className="text-center text-lg md:text-xl lg:text-2xl xl:text-lg font-Noto text-[#67738e] pb-14">
           I'm currently actively seeking new opportunities, and my inbox is
           always open. Whether you'd like to discuss something specific or
           simply reach out, I'll make every effort to respond promptly.
