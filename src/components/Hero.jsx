225736919f7c6046d0d36d243312c7bfb2832654
NARD <glenardrefamonte1@gmail.com>
2024-03-10 22:53:44 +0800

    Feat: Added skleton for loading effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TechStack from "./TechStack";
+import { CiImageOn } from "react-icons/ci";
 const Hero = () => {
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <>
       <div className="hero min-h-screen sm:px-32 font-bold font-Noto">
         <div className="hero-content flex-col lg:flex-row-reverse  ">
-          <img
-            src="/profile-pic.png"
-            alt="profile"
-            // className=" rounded-full border-black border-4 brightness-75 hover:brightness-100"
-            className="sm:max-w-sm xs:max-w-auto h-[400px]  rounded-lg  brightness-75 hover:brightness-100"
-          />
+          {loading ? (
+            <img
+              src="/no-image.jpg"
+              alt="profile"
+              // className=" rounded-full border-black border-4 brightness-75 hover:brightness-100"
+              className="sm:max-w-sm xs:max-w-auto h-[400px] animate-pulse rounded-full brightness-75 hover:brightness-100"
+            />
+          ) : (
+            <img
+              src="/profile-pic.png"
+              alt="profile"
+              // className=" rounded-full border-black border-4 brightness-75 hover:brightness-100"
+              className="sm:max-w-sm xs:max-w-auto h-[400px]  rounded-lg  brightness-75 hover:brightness-100"
+            />
+          )}
           <div>
             <p className="text-3xl  font-Mono pl-3">Hi, my name is</p>
             <h1 className="text-6xl leading-normal text-[#ccd6f6]">
@@ -30,3 +47,11 @@ const Hero = () => {
 };
 
 export default Hero;
+
+const Skeleton = () => {
+  return (
+    <div className="sm:max-w-sm  flex items-center text-gray-500 justify-center text-8xl rounded-full xs:max-w-auto h-[400px] bg-[#e4e5ea] animate-pulse  ">
+      <CiImageOn />
+    </div>
+  );
+};
