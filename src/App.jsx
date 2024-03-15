4623bfb0fae8de8a3aeecec1192875f95735324f
NARD <glenardrefamonte1@gmail.com>
2024-03-15 18:26:49 +0800

    added loading effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Repository from "./components/pages/Repository";
 import Home from "./components/Home";
+import Loader from "./components/Loader";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/repository" element={<Repository />} />
-        </Routes>
-      </BrowserRouter>
+      {loading ? (
+        <Loader />
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/repository" element={<Repository />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </>
   );
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div class="wrapper h-screen">
+      <svg>
+        <text x="50%" y="50%" dy=".35em" text-anchor="middle">
+          Glenard Becerro
+        </text>
+      </svg>
+    </div>
+  );
+};
+
+export default Loader;
