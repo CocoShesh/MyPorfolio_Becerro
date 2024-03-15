7c90996f7c13d3cd1bb39a89b20a098744df3054
NARD <glenardrefamonte1@gmail.com>
2024-03-15 22:32:36 +0800

    try skeleton

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import TechStack from "./TechStack";
+
+// Placeholder image URL
+const placeholderImage = "https://fakeimg.pl/600x400?text=Nard&font=bebas";
+
 const Hero = () => {
+  // State to track whether the image has loaded
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  // Function to handle image load event
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <>
       <div className="hero min-h-screen sm:px-32 font-bold font-Noto">
         <div className="hero-content flex-col lg:gap-28 lg:flex-row-reverse  ">
+          {/* Conditional rendering based on image load status */}
+          {!imageLoaded && (
+            <img
+              src={placeholderImage}
+              alt="placeholder"
+              className="sm:max-w-sm xs:max-w-auto rounded-lg brightness-75"
+            />
+          )}
           <img
             src="/profile-pic.png"
             alt="profile"
-            className="sm:max-w-sm xs:max-w-auto  rounded-lg  brightness-75 hover:brightness-100"
+            className={`sm:max-w-sm xs:max-w-auto rounded-lg brightness-75 hover:brightness-100 ${
+              imageLoaded ? "visible" : "hidden"
+            }`}
+            onLoad={handleImageLoad}
           />
           <div>
-            <p className="text-3xl  font-Mono pl-3">Hi, my name is</p>
+            <p className="text-3xl font-Mono pl-3">Hi, my name is</p>
             <h1 className="text-6xl leading-normal text-[#ccd6f6]">
               Glenard Becerro
             </h1>
