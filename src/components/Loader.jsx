8b9c3af6201005a5e7193fbd39867b512a0cc0eb
NARD <glenardrefamonte1@gmail.com>
2024-03-15 18:36:06 +0800

    make the loading responsive

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div class="wrapper h-screen">
+    <div className="wrapper h-screen">
       <svg>
-        <text x="50%" y="50%" dy=".35em" text-anchor="middle">
+        <text
+          x="50%"
+          y="50%"
+          dy=".35em"
+          textAnchor="middle"
+          className="responsive-text"
+        >
           Glenard Becerro
         </text>
       </svg>
