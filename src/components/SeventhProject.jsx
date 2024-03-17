cfb6ca0fa77b06c76dfc9be8dee715f4e0407c72
NARD <glenardrefamonte1@gmail.com>
2024-03-17 22:48:04 +0800

    updated
    fix the position

--- src/components/SeventhProject.jsx
+++ src/components/SeventhProject.jsx
@@ -8,29 +8,19 @@ const SeventhProject = () => {
   }, []);
   return (
     <>
-      <section className=" flex  flex-col mx-auto max-xl:hidden   gap-20  mt-20  px-44 justify-center items-center scroll-smooth">
-        <section className="flex ">
-          <a href="https://luxury-cars-rental.netlify.app/" target="_blank">
-            <img
-              data-aos="fade-right"
-              data-aos-offset="300"
-              data-aos-easing="ease-in-sine"
-              src="/car-rental.jpg"
-              alt=""
-              className="w-[600px] h-[350px]   shadow-custom rounded-md "
-            />
-          </a>
+      <section className=" flex  items-center justify-center mt-10  mx-auto  max-xl:hidden   gap-20   px-44 scroll-smooth">
+        <section className="flex justify-center items-center ">
           <section
-            data-aos="fade-left"
+            data-aos="fade-right"
             data-aos-offset="300"
             data-aos-easing="ease-in-sine"
-            className=" text-right relative w-[600px] 2xl:w-[500px] h-auto"
+            className=" text-left relative w-[600px] 2xl:w-[500px] h-auto "
           >
             <p className="font-Mono text-[#9ca7c6]">Featured Project</p>
             <h1 className=" font-Noto text-3xl font-bold text-[#64ffda]">
               Car Rental
             </h1>
-            <section className="w-[600px] h-[130px] bg-[#112240] mt-5 p-5 absolute right-0 font-Mono">
+            <section className="w-[600px] h-[120px] z-10 bg-[#112240] mt-5 p-5  absolute left-0 font-Mono  text-orange-200 text-justify text-[15px] font-bold">
               <p className="text-justify font-bold font-Mono  text-orange-300">
                 Discover the latest releases and timeless classics. Get ready to
                 embark on thrilling journeys and experience gaming like never
@@ -39,12 +29,12 @@ const SeventhProject = () => {
               </p>
             </section>
             <section className="mt-52  ">
-              <ul className="flex justify-end gap-5 font-Mono uppercase text-[#9ca7c6] ">
+              <ul className="flex gap-5 font-Mono uppercase text-[#9ca7c6] ">
                 <li>React Js</li>
                 <li>Tailwind Css</li>
               </ul>
             </section>
-            <section className="flex gap-5 justify-end mt-5 text-2xl">
+            <section className="flex gap-5 mt-5 text-2xl">
               <a
                 href=" https://github.com/CocoShesh/Car-Rental-App"
                 target="_blank"
@@ -58,6 +48,16 @@ const SeventhProject = () => {
               </a>
             </section>
           </section>
+          <a href="https://luxury-cars-rental.netlify.app/" target="_blank">
+            <img
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              src="/car-rental.jpg"
+              alt=""
+              className="w-[600px] h-[350px]   shadow-custom rounded-md "
+            />
+          </a>
         </section>
       </section>
 
