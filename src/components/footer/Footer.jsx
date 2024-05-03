516ee5f888d9046651f47fb067fff3720a5b9be6
NARD <glenardrefamonte1@gmail.com>
2024-05-04 02:24:16 +0800

    Update: Change font family, import, and apply in the body

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <section className="flex flex-col items-center justify-center mt-14 font-Mono pb-10 text-sm text-[#9ca7c6]">
+      <section className="flex flex-col items-center justify-center mt-14  pb-10 text-sm text-[#9ca7c6]">
         <p>Designed & Built by Glenard Becerro</p>
         <p>October/26/2023</p>
       </section>
