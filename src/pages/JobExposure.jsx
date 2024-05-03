1a3ea2d7e553ad8aecd083ef430984fba0bfb77c
NARD <glenardrefamonte1@gmail.com>
2024-05-04 01:49:50 +0800

    Resolve issue with image display: Fix error in rendering image

---
+++ src/pages/JobExposure.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const JobExposure = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col items-center justify-center">
+      {" "}
+      this is job exposure
+    </section>
+  );
+};
+
+export default JobExposure;
