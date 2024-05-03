ad1d24285cd97635b4c374be658e265b690d8688
NARD <glenardrefamonte1@gmail.com>
2024-05-03 18:03:38 +0800

    Created a data file to store information for all projects

---
+++ src/components/project/more/Data.js
@@ -0,0 +1,151 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Furniture Shop",
+    liveLink: "https://furnicraft-v1.netlify.app/",
+    githubLink: "https://github.com/CocoShesh/FurniCraft",
+    src: "/furniture.png",
+    descriptions:
+      " A virtual furniture shop providing an extensive collection of  handcrafted furniture pieces, blending modern aesthetics withmtimeless craftsmanship. Immerse yourself in a world of quality and comfort, where each piece reflects our commitment to exceptional design and functionality.",
+    techStack: ["React Js", "Tailwind Css"],
+  },
+  {
+    id: 2,
+    title: "Car Rental",
+    liveLink: "https://luxury-cars-rental.netlify.app/",
+    githubLink: "https://github.com/CocoShesh/Car-Rental-App ",
+    src: "/car-rental.jpg",
+    descriptions:
+      "Discover the latest releases and timeless classics. Get ready to embark on thrilling journeys and experience gaming like never before. Start exploring and elevate your gaming experience today!",
+    techStack: ["React Js", "Tailwind Css"],
+  },
+  {
+    id: 3,
+    title: " Dictionary App",
+    liveLink: "https://dictionary-app-created-nard.netlify.app",
+    githubLink: "https://github.com/CocoShesh/Dictionary-app",
+    src: "/dictionary.png",
+    descriptions:
+      "Dictionary is a project where I practice working with APIs. Users can explore words, customize fonts and themes to their liking, and listen to word pronunciations, making it a comprehensive tool for language enthusiasts and learners alike.",
+    techStack: ["React Js", "Tailwind Css", "Axios"],
+  },
+  {
+    id: 4,
+    title: "Cinema",
+    liveLink: "https://movie-cinema-liart.vercel.app/",
+    githubLink: "https://github.com/CocoShesh/MovieCinema ",
+    src: "/preview.jpg",
+    descriptions:
+      "Welcome to Cinema! Immerse yourself in a world of cinematic wonders, where stories come alive and emotions unfold on the big screen.  Join us for an unforgettable experience filled with laughter, tears, and edge-of-your-seat moments",
+    techStack: ["React Js", "Tailwind Css"],
+  },
+  {
+    id: 5,
+    title: "Ip Address Tracker",
+    liveLink: "https://simple-ip-address-tracker.netlify.app/",
+    githubLink: "https://github.com/CocoShesh/IP-Address-Tracker",
+    src: "/IP-TRACKER-PREVIEW.png",
+    descriptions:
+      "Build a user-friendly IP Address Tracker web app using React Js, integrating the IP Geolocation API by IPify and LeafletJS for interactive map functionality, ensuring seamless user experience across devices.",
+    techStack: ["React Js", "Tailwind Css", "LeafletJS", "IP Geolocation"],
+  },
+  {
+    id: 6,
+    title: "Weather App",
+    liveLink: "https://weather-app-nard.netlify.app/",
+    githubLink: "https://github.com/CocoShesh/Weather_App",
+    src: "/Weather-Preview.png",
+    descriptions:
+      "My weather app, utilizing weatherapi.com, provides current weather and weekly forecasts. offering toggling between Celsius and Fahrenheit, alongside a dark mode option for comfortable viewing in any lighting. ",
+    techStack: ["React Js", "Tailwind Css", "Api"],
+  },
+  {
+    id: 7,
+    title: "Todo App",
+    liveLink: "https://todo-app-nard.netlify.app/",
+    githubLink: "https://github.com/CocoShesh/Todo-App",
+    src: "/desktop-preview.jpg",
+    descriptions:
+      "This efficient Todo app empowers users to seamlessly organize their tasks by enabling them to add, complete, and delete todos.  Additionally, users can effortlessly filter tasks based on their status, switch between light and dark modes, and enjoy various other features for enhanced task management.",
+    techStack: ["React Js", "Tailwind Css", "uuid", "Visual Studio Code"],
+  },
+  {
+    id: 8,
+    title: "Country App",
+    liveLink: "https://countries-nard.netlify.app/",
+    githubLink: "https://github.com/CocoShesh/Country-App",
+    src: "/County_App_Preview.png",
+    descriptions:
+      "Build a user-friendly IP Address Tracker web app using React Js, integrating the IP Geolocation API by IPify and LeafletJS for interactive map functionality, ensuring seamless user experience across devices. ",
+    techStack: ["React Js", "Tailwind Css", "Lazy loading"],
+  },
+  {
+    id: 9,
+    title: "Mind Quiz",
+    liveLink: "https://mind-quiz-v1.netlify.app/",
+    githubLink: "https://github.com/CocoShesh/Mind_Quiz",
+    descriptions:
+      "This interactive platform is built with React, Tailwind CSS, and an API fusion to deliver an immersive quiz experience. Dive in and explore the fascinating realm of coding challenges tailored just for you. ",
+    techStack: ["React js", "Tailwind CSS", "Context Api", "Axios"],
+  },
+  {
+    id: 10,
+    title: "Multi Step Form",
+    liveLink: "https://multi-step-form-sample.netlify.app/",
+    githubLink: " https://github.com/CocoShesh/Multi-Step-Form",
+    src: "/multi-step-preview.png",
+    descriptions:
+      "Build a multi-step form, closely matching the provided design, allowing users to progress through each step, review and edit their selections, and confirm their order",
+    techStack: ["React Js", "Tailwind Css", "Zod", "Context Api"],
+  },
+  {
+    id: 11,
+    title: "Tip Calculator",
+    liveLink: "https://tip-calculator-app-tau.vercel.app/",
+    githubLink: "https://github.com/CocoShesh/tip-calculator-app-main",
+    src: "/PREVIEW-3.png",
+    descriptions:
+      "A tip calculator helps determine the appropriate gratuity for a bill. Users input the total amount, select a tip percentage, and specify the number of people splitting the bill. The calculator computes the tip, adds it to the bill, and divides the total among the group. It simplifies fair tip calculation when dining out or sharing expenses.",
+    techStack: ["Html", "Vanilla Css", "Javascript", "Api"],
+  },
+  {
+    id: 12,
+    title: " Games Store",
+    liveLink: "https://game-app-cocoshesh.vercel.app/",
+    githubLink: "https://github.com/CocoShesh/Game-App",
+    src: "/games-preview.png",
+    descriptions:
+      "Discover the latest releases and timeless classics. Get ready to embark on thrilling journeys and experience gaming like never before. Start exploring and elevate your gaming experience today!",
+    techStack: ["React Js", "Tailwind Css"],
+  },
+  {
+    id: 13,
+    title: "Timer App",
+    liveLink: "https://simple-timer-by-nard.netlify.app/",
+    githubLink: "https://github.com/CocoShesh/Timer-App",
+    src: "/timer.png",
+    descriptions:
+      " This sleek Timer app allows users to efficiently manage their time by providing essential timing features. Users can start, stop, and reset the timer, making it a handy tool for various activities. Whether you're working, studying, or simply keeping track of tasks, this Timer App has you covered.",
+    techStack: ["React Js", "Tailwind Css"],
+  },
+  {
+    id: 14,
+    title: "Corny Jokes Generator",
+    liveLink: "https://corny-joke-generator.vercel.app/",
+    githubLink: "https://github.com/CocoShesh/Corny-Jokes-Generator/tree/main",
+    src: "/Corny-Jokes-Preview.png",
+    descriptions:
+      "Corny-Jokes-Generator is a whimsical project designed to bring laughter to users with its quirky algorithm that generates an endless array of light-hearted and amusing jokes. From puns to playful one-liners, it promises to be a delightful source of entertainment for all ages.",
+    techStack: ["React Js", "Tailwind Css"],
+  },
+  {
+    id: 15,
+    title: " Youtube Clone",
+    liveLink: "https://youtube-clone-created-nard.netlify.app",
+    githubLink: "https://github.com/CocoShesh/youtube-client",
+    src: "/youtube-preview.png",
+    descriptions:
+      "  I built this platform to hone my skills, particularly tackling the challenge of designing a responsive sidebar that adapts to various screen sizes. Despite the initial hurdles, I successfully completed the project, refining my abilities along  the way.",
+    techStack: ["React Js", "Tailwind Css", "Node Js", "Express Js", "MongoDB"],
+  },
+];
