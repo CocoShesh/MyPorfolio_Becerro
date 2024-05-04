ba880a9e65312f39ec726101e22d6cd22568d474
NARD <glenardrefamonte1@gmail.com>
2024-05-05 01:47:35 +0800

    Remove unnecessary imported components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,17 +4,6 @@ import Hero from "./hero/Hero";
 import SocialMediaAccounts from "./social_media/SocialMediaAccounts";
 import About from "./about/About";
 import Education from "./education/Education";
-import FurnitureProject from "./project/Furniture_Shop.jsx";
-import CinemaProject from "./project/Cinema.jsx";
-import WeatherAppProject from "./project/Weather_App";
-import TipCalculatorProject from "./project/Tip_Calculator";
-import GameStoreProject from "./project/Game_Store";
-import TodoAppProject from "./project/Todo_App";
-import CarRentalProject from "./project/Car_Rental.jsx";
-import TimerAppProject from "./project/Timer_App";
-import DictionaryAppProject from "./project/Dictionary_App.jsx";
-import CountryAppProject from "./project/Country_Appt.jsx";
-import MoreProjects from "./project/more/MoreProjects.jsx";
 import MyRepository from "./repository/MyRepository";
 import Contact from "./contact/Contact";
 import Footer from "./footer/Footer";
@@ -30,17 +19,6 @@ const Home = () => {
         <SocialMediaAccounts />
         <About />
         <Education />
-        {/* <FurnitureProject />
-        <CinemaProject />
-        <DictionaryAppProject />
-        <TodoAppProject />
-        <TimerAppProject />
-        <CarRentalProject />
-        <WeatherAppProject />
-        <TipCalculatorProject />
-        <GameStoreProject />
-        <CountryAppProject />
-        <MoreProjects /> */}
         <Sample />
         <MyRepository />
         <Contact />
