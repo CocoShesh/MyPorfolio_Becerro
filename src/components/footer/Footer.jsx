ff93e224aaa2694d4f70b86ed7fe0237a5ace288
NARD <glenardrefamonte1@gmail.com>
2024-05-05 01:48:39 +0800

    Feat: Remove the useEffect import in custom hooks for useAnimations, then fix the alignment of the content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <>
-      <section className="flex flex-col items-center justify-center mt-14  pb-10 text-sm text-[#9ca7c6]">
-        <p>Designed & Built by Glenard Becerro</p>
-        <p>October/26/2023</p>
-      </section>
-    </>
+    <footer className="flex flex-col items-center justify-center mt-14 pb-5 text-sm text-[#9ca7c6]">
+      <p>Designed and Built by Glenard Becerro</p>
+      <p>Inspired by the Design of Brittany Chiang</p>
+      <p>October 26, 2023</p>
+    </footer>
   );
 };
 
