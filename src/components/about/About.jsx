151fec49fa34f521880e21dec469865871a711b8
NARD <glenardrefamonte1@gmail.com>
2024-05-05 01:48:28 +0800

    Feat: Remove the useEffect import in custom hooks for useAnimations, then fix the alignment of the content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,57 +1,63 @@
-import React, { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import useAOS from "../../hooks/UseAnimations";
 const About = () => {
-  useEffect(() => {
-    AOS.init();
-  }, []);
+  useAOS();
   return (
     <>
-      <section
-        data-aos="fade-right"
-        data-aos-offset="300"
-        data-aos-easing="ease-in-sine"
-        className="flex items-center sm:px-36 gap-3 text-3xl py-16 max-sm:pl-5 "
-        id="about"
-      >
-        <section className=" flex gap-3 2xl:mx-auto">
-          <span> 02. </span>
-          <h1 className="  font-bold text-[#ccd6f6]"> About Me. </h1>
-        </section>
-      </section>
-      <div className="hero  sm:px-32  font-bold ">
-        <div className="hero-content flex-col lg:flex-row gap-20 ">
-          <img
+      <section className="w-full flex flex-col justify-center items-center ">
+        <section className="flex items-start flex-col">
+          <section
             data-aos="fade-right"
             data-aos-offset="300"
             data-aos-easing="ease-in-sine"
-            src="/Images/second_pic.jpg"
-            alt=""
-            className="sm:max-w-sm xs:max-w-auto   rounded-lg  brightness-75 hover:brightness-100"
-          />
-
-          <div
-            data-aos="fade-left"
-            data-aos-offset="300"
-            data-aos-easing="ease-in-sine"
+            className="flex items-center sm:px-36 gap-3 text-3xl py-16 max-sm:pl-5 "
+            id="about"
           >
-            <p className="text-lg  text-justify text-[#67738e]">
-              As a fourth-year student with a foundational understanding of web
-              development, seeking an internship opportunity where I can apply
-              my current skills and gain hands-on experience in a professional
-              setting, my knowledge, which I've acquired from various YouTube
-              videos, tutorials, and documentation, has significantly
-              contributed to my ability to understand and apply web development
-              concepts. I am motivated to contribute to real-world projects,
-              learn from experienced professionals, and enhance my web
-              development capabilities while making a meaningful impact within
-              the company.
-            </p>
-            {/* <TechStack /> */}
+            <section className="flex gap-3">
+              <span> 02. </span>
+              <h1 className="font-bold text-[#ccd6f6]"> About Me. </h1>
+            </section>
+          </section>
+
+          <div className="hero sm:px-32 font-bold">
+            <div className="hero-content flex-col lg:flex-row gap-20">
+              <img
+                data-aos="fade-right"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine"
+                src="/Images/second_pic.jpg"
+                alt=""
+                className="sm:max-w-sm xs:max-w-auto rounded-lg brightness-75 hover:brightness-100"
+              />
+
+              <div
+                data-aos="fade-left"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine"
+              >
+                <p className="text-[16px] 2xl:text-lg indent-10 text-justify text-[#67738e]">
+                  As a fourth-year student deeply passionate about web
+                  development, I am currently seeking a job opportunity to apply
+                  and further develop my skills in a professional environment.
+                  With a solid foundation nurtured through academic coursework
+                  and self-directed learning from diverse resources, such as
+                  YouTube videos, tutorials, and documentation, I am equipped
+                  with a versatile toolkit to tackle challenges in the field.
+                </p>
+                <br />
+                <p className="text-[16px] 2xl:text-lg indent-10 text-justify text-[#67738e]">
+                  Driven by curiosity and a hunger for hands-on experience, I am
+                  enthusiastic about applying my knowledge to real-world
+                  projects and collaborating with experienced professionals. My
+                  goal is not only to enhance my technical proficiency but also
+                  to make a tangible impact within the organization, leveraging
+                  creativity and problem-solving skills to deliver innovative
+                  solutions.
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      {/* </section> */}
+        </section>
+      </section>
     </>
   );
 };
