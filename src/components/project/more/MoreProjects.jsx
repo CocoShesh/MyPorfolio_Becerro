2648455be1af7a4d6aa0d1a08d359e6531c59ba2
NARD <glenardrefamonte1@gmail.com>
2024-05-05 01:52:51 +0800

    Feat: Remove the useEffect import in custom hooks for useAnimations, then fix the alignment of the content

--- src/components/project/more/MoreProjects.jsx
+++ src/components/project/more/MoreProjects.jsx
@@ -1,27 +1,83 @@
-import React, { useEffect, useState } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import IpAddressTracker from "./Ip_Address_Tracker";
-import YoutubeClone from "./Youtube_Clone";
-import SocialLinksProfile from "./Social_Links_Profile";
-import MultiFormState from "./Multi_Form_State";
-import CornyJokeGenerator from "./Corny_Jokes_Generator";
-const MoreProjects = () => {
-  const [toggleViewMore, setToggleViewMore] = useState(false);
-  useEffect(() => {
-    AOS.init();
-  }, []);
+import { projects } from "../Data";
+import { PiGithubLogoBold } from "react-icons/pi";
+import { FaRegShareFromSquare } from "react-icons/fa6";
+const MoreProjects = ({ toggleViewMore, setToggleViewMore }) => {
   return (
     <>
-      {toggleViewMore && (
-        <>
-          <IpAddressTracker />
-          <YoutubeClone />
-          <SocialLinksProfile />
-          <MultiFormState />
-          <CornyJokeGenerator />
-        </>
-      )}
+      {toggleViewMore &&
+        projects.slice(10, 15).map((project, index) => {
+          const isEven = project.id % 2 === 1;
+          return (
+            <>
+              <section
+                key={index}
+                className={`flex flex-col mx-auto max-xl:hidden gap-20 mt-20   px-44 justify-center items-center scroll-smooth `}
+              >
+                <section className={`flex  ${isEven && "flex-row-reverse"}`}>
+                  <a href={project.liveLink} target="_blank">
+                    <img
+                      data-aos="fade-right"
+                      data-aos-offset="300"
+                      data-aos-easing="ease-in-sine"
+                      src={project.src}
+                      alt=""
+                      className="w-[600px] h-[350px]  shadow-custom rounded-md"
+                    />
+                  </a>
+                  <section
+                    data-aos="fade-left"
+                    data-aos-offset="300"
+                    data-aos-easing="ease-in-sine"
+                    className={` relative   ${
+                      isEven ? "text-left" : "text-right"
+                    }  relative w-[600px] 2xl:w-[500px] h-auto`}
+                  >
+                    <p className=" text-[#9ca7c6]">Featured Project</p>
+                    <h1 className=" text-3xl font-bold text-[#64ffda]">
+                      {project.title}
+                    </h1>
+                    <section
+                      className={`w-[600px] h-fit bg-[#112240] mt-5 p-5 absolute ${
+                        isEven ? "left-0" : "right-0"
+                      } `}
+                    >
+                      <p
+                        className={` font-bold text-sm  text-[#64ffda] tracking-wider leading-6 brightness-75  ${
+                          isEven ? "text-left" : "text-right"
+                        }`}
+                      >
+                        {project.descriptions}
+                      </p>
+                    </section>
+                    <section className="mt-52">
+                      <ul
+                        className={`flex  ${
+                          isEven ? "justify-start" : "justify-end"
+                        } gap-5  uppercase text-[#9ca7c6]`}
+                      >
+                        {project.techStack.map((stack, index) => {
+                          return <li key={index}>{stack}</li>;
+                        })}
+                      </ul>
+                    </section>
+                    <section
+                      className={`flex gap-5 ${
+                        isEven ? "justify-start" : "justify-end"
+                      }  mt-5 text-2xl`}
+                    >
+                      <a href={project.githubLink} target="_blank">
+                        <PiGithubLogoBold />
+                      </a>
+                      <a href={project.liveLink} target="_blank">
+                        <FaRegShareFromSquare />
+                      </a>
+                    </section>
+                  </section>
+                </section>
+              </section>
+            </>
+          );
+        })}
       <section className="mt-20 flex flex-col justify-center items-center">
         <button
           onClick={() => setToggleViewMore(!toggleViewMore)}
