a6787d6f25c8c62f3f011e1105b663904611b19a
NARD <glenardrefamonte1@gmail.com>
2024-05-06 03:17:54 +0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Repository from "./pages/Repository";
 import Home from "./components/Home";
 import Loader from "./components/loader/Loader";
-import Songs from "./pages/Songs";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -25,7 +24,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/repository" element={<Repository />} />
-            <Route path="/songs" element={<Songs />} />
             {/* <Route path="/job-exposure" element={<JobExposure />} /> */}
           </Routes>
         </BrowserRouter>
