e1af627e9cd63c90b0a8d909541e63862f3e8002
NARD <glenardrefamonte1@gmail.com>
2024-05-06 01:30:44 +0800

    Updated

--- src/pages/JobExposure.jsx
+++ src/pages/JobExposure.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const JobExposure = () => {
   return (
     <section className="min-h-screen w-full flex flex-col items-center justify-center">
-      {" "}
       this is job exposure
     </section>
   );
