6802b4c4b00875fdebd063026811721e87345f96
NARD <glenardrefamonte1@gmail.com>
2024-07-25 00:14:49 +0800

    Updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -43,23 +43,16 @@ const About = () => {
                   nurtured through academic coursework and self-directed
                   learning from diverse resources, such as YouTube video*
                   tutorials, and documentation I am equipped with a versatile
-                  toolkit to tackle challenges in the field. Driven by curiosity
-                  and a hunger for I am enthusiastic about applying my knowledge
-                  to real-world projects and collaborating with experienced
-                  professionals. My goal is not only to enhance my technical
-                  proficiency but also to make a tangible impact within the
-                  organization. leveraging creativity and problem-solving skills
-                  to deliver innovative solutions.
+                  toolkit to tackle challenges in the field.
                 </p>
                 <br />
                 <p className="text-[16px] 2xl:text-lg indent-10 text-justify text-[#67738e]">
-                  Driven by curiosity and a hunger for hands-on experience, I am
-                  enthusiastic about applying my knowledge to real-world
-                  projects and collaborating with experienced professionals. My
-                  goal is not only to enhance my technical proficiency but also
-                  to make a tangible impact within the organization, leveraging
-                  creativity and problem-solving skills to deliver innovative
-                  solutions.
+                  Driven by curiosity and a hunger for I am enthusiastic about
+                  applying my knowledge to real-world projects and collaborating
+                  with experienced professionals. My goal is not only to enhance
+                  my technical proficiency but also to make a tangible impact
+                  within the organization. leveraging creativity and
+                  problem-solving skills to deliver innovative solutions.
                 </p>
               </div>
             </div>
